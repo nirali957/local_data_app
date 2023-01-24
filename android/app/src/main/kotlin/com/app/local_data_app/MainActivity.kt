2944aacd280ea69d0b8c5940b0b2eaaaa01eafd0
Nirali Dhorajiya <niralidhorajiya123@gmail.com>
2023-01-24 09:01:55 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/app/local_data_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.app.local_data_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/image_screen.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImageScreen extends StatefulWidget {
+  const ImageScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ImageScreen> createState() => _ImageScreenState();
+}
+
+class _ImageScreenState extends State<ImageScreen> {
+  final ImagePicker picker = ImagePicker();
+
+  XFile? imageFile;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Image Picker Screen"),
+      ),
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              imageFile == null
+                  ? const SizedBox()
+                  : Image.file(
+                      File(imageFile!.path),
+                    ),
+              ElevatedButton(
+                onPressed: () async {
+                  imageFile = await picker.pickImage(
+                    source: ImageSource.camera,
+                    imageQuality: 100,
+                  );
+                  setState(() {});
+                  debugPrint("imageFile!.path ------------>>> ${imageFile!.path}");
+                },
+                child: const Text("Select"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
