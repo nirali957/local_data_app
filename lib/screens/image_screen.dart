98fc7d5381b9d7081c836662fb2ac73e69667c0a
Nirali Dhorajiya <niralidhorajiya123@gmail.com>
2023-01-25 10:20:10 +0530

    data1

--- lib/screens/image_screen.dart
+++ lib/screens/image_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ImageScreen extends StatefulWidget {
